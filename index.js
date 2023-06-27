900ea9ee73e91773eb803b1e606726b00db75e2a
Daniel Ariaz-Avalos <dannyariaz45@gmail.com>
2023-06-26 20:19:44 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// var generateName = require("sillyname");
+// var sillyName = generateName();
+
+// console.log("My name is " + sillyName);
+
+import superheroes from "superheroes";
+
+console.log("I am " + superheroes.random());
